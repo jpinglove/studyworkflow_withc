3e412f9e37c3bb906afbf60accf4f5065be822b8
jpinglove <jpinglove@gmail.com>
2024-11-04 10:43:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
 // Your First C++ Program
-
+/*
 #include <iostream>
 
 int main() {
     std::cout << "Hello World!";
     return 0;
 }
+*/
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
+}
+
